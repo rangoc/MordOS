40222141a85df6325cb876b44534d22f3c0b793a
goranc <cabarkapa.goran@gmail.com>
2021-06-19 08:25:44 +0200

    Added context & initial styling

---
+++ src/components/ui/UserInterface.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import './userInterface.scss';
+const UserInterface = () => {
+  return <div className="ui">Hello I am user interface</div>;
+};
+
+export default UserInterface;
---
+++ src/context/AuthProvider.js
@@ -0,0 +1,33 @@
+import React, { useState, createContext } from 'react';
+
+const AuthContext = createContext({
+  email: '',
+  password: '',
+  isAuthenticated: false,
+});
+
+const AuthProvider = ({ children }) => {
+  const [formValues, setFormValues] = useState({
+    email: '',
+    password: '',
+  });
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        formValues,
+        isAuthenticated,
+        setIsAuthenticated,
+        setFormValues,
+        handleChange,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthProvider, AuthContext };
