2fc5a719d640fc78d24f9af377c0337a1e62edcd
goranc <cabarkapa.goran@gmail.com>
2021-06-24 15:31:16 +0200

    Retrieving the working commit

--- src/components/ui/UserInterface/UserInterface.js
+++ src/components/ui/UserInterface/UserInterface.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useContext } from 'react';
 // components
-import AppBar from 'components/appbar/AppBar';
-import Logo from './Logo';
-import CreateTextFile from 'components/textFile/CreateTextFile';
-import FileDirectory from 'components/fileDirectory/FileDirectory';
-import EditTextFile from 'components/textFile/EditTextFile';
-import Gallery from 'components/gallery/Gallery';
-import RssFeed from 'components/rssFeed/RssFeed';
+import AppBar from 'components/appbar/AppBar/AppBar';
+import Logo from '../Logo/Logo';
+import CreateTextFile from 'components/textFile/CreateTextFile/CreateTextFile';
+import FileDirectory from 'components/fileDirectory/FileDirectory/FileDirectory';
+import EditTextFile from 'components/textFile/EditTextFile/EditTextFile';
+import Gallery from 'components/gallery/Gallery/Gallery';
+import RssFeed from 'components/rssFeed/RssFeed/RssFeed';
 import Webcam from 'components/webcam/Webcam';
 import WebBrowser from 'components/webBrowser/WebBrowser';
 // context
@@ -14,7 +14,6 @@ import { WindowContext } from 'context/WindowProvider';
 import { TextFileContext } from 'context/TextFileProvider';
 // sass
 import './userInterface.scss';
-
 const UserInterface = () => {
   const { isOpen } = useContext(WindowContext);
   const { file } = useContext(TextFileContext);
